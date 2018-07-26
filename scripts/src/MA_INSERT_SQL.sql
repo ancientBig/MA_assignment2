f798a7a013ad4460e853e1fad3972e3264513e7c
Xaba <morena.a.xaba@accenture.com>
2018-07-26 08:43:59 +0200

    add the bridge table

--- scripts/src/MA_INSERT_SQL.sql
+++ scripts/src/MA_INSERT_SQL.sql
@@ -1,16 +1,46 @@
 
 USE MA_assignment;
 
-INSERT INTO courses (crs_name, crs_duration,crs_cost) VALUES('CALCULUS',2,4000 );
-INSERT INTO courses (crs_name, crs_duration,crs_cost) VALUES('LITERATURE',1,3500 );
-INSERT INTO courses (crs_name, crs_duration,crs_cost) VALUES('ASTRONOMY',4,5000 );
-INSERT INTO courses (crs_name, crs_duration,crs_cost) VALUES('Accenture Java Training',7,10000 );
+
+INSERT INTO users (user_name, user_email,  user_password, user_type,  user_RegisterDate,  user_address) VALUES('MOSHI','m@accenture.com','12345',1,str_to_date('05-11-2004','%m-%d-%Y'),'64 boine,bloeamfontein');
+INSERT INTO users (user_name, user_email,  user_password, user_type,  user_RegisterDate,  user_address) VALUES('Moloi','moloi@accenture.com','12345',1, str_to_date('02-22-1994','%m-%d-%Y'),'63 vilakazi str, SOWETO');
+INSERT INTO users (user_name, user_email,  user_password, user_type,  user_RegisterDate,  user_address) VALUES('Sipho','si@accenture.com',1, str_to_date('07-02-2003','%m-%d-%Y'),'11 plain street, Johannesburg');
+INSERT INTO users (user_name, user_email,  user_password, user_type,  user_RegisterDate,  user_address) VALUES('MALETSATSI','maletsi@accenture.com',1, str_to_date('02-02-2001','%m-%d-%Y'),'11 plain street, Soweto');
+
+INSERT INTO users (user_name, user_email,  user_password, user_type,  user_RegisterDate,  user_address) VALUES('Morena','morena@accenture.com',2, str_to_date('01-02-2007','%m-%d-%Y'),'122 Thulani Str, Katlehong');
+INSERT INTO users (user_name, user_email,  user_password, user_type,  user_RegisterDate,  user_address) VALUES('Oupa','si@accenture.com',2, str_to_date('06-15-2011','%m-%d-%Y'),'100 vein Str, Johannesburg');
+INSERT INTO users (user_name, user_email,  user_password, user_type,  user_RegisterDate,  user_address) VALUES('Mpho','si@accenture.com',2, str_to_date('08-20-2012','%m-%d-%Y'),'44 Halves Str, Maboneng');
 
 
-INSERT INTO teachers (course_id, tch_name,tch_lastName,tch_Address) VALUES(1,'MOSHI','Xholo','64 boine,bloeamfontein');
-INSERT INTO teachers (course_id, tch_name,tch_lastName,tch_Address) VALUES(2,'Moloi','tshabalala','63 vilakazi str, SOWETO' );
-INSERT INTO teachers (course_id, tch_name,tch_lastName,tch_Address) VALUES(3,'Sipho','thabetha','11 plain street, Johannesburg');
 
-INSERT INTO students (course_id, teacher_id,std_name,std_lastName,std_address) VALUES(1,1,'Morena','xaba','122 Thulani Str, Katlehong' );
-INSERT INTO students (course_id, teacher_id,std_name,std_lastName,std_address) VALUES(2,2,'Oupa','toko','100 vein Str, Johannesburg' );
-INSERT INTO students (course_id, teacher_id,std_name,std_lastName,std_address) VALUES(3,3,'Mpho','malachi','44 Halves Str, Maboneng' );
+INSERT INTO teachers (user_id, teacher_CareerLevel, teacher_DEPT) VALUES(1,1,'SCIENCE');
+INSERT INTO teachers (user_id, teacher_CareerLevel, teacher_DEPT) VALUES(2,1,'MATHEMATICS');
+INSERT INTO teachers (user_id, teacher_CareerLevel, teacher_DEPT) VALUES(3,2,'TEACHING');
+INSERT INTO teachers (user_id, teacher_CareerLevel, teacher_DEPT) VALUES(4,3,'IT');
+
+
+INSERT INTO students (user_id, student_dept) VALUES(5,1,'IT' );
+INSERT INTO students (user_id, student_dept) VALUES(6,2,'MATHEMATICS');
+INSERT INTO students (user_id, student_dept) VALUES(7,3,'SCIENCE');
+
+INSERT INTO courses (teacher_id,crs_name, crs_duration,crs_cost) VALUES(4,'CALCULUS',2,4000 );
+INSERT INTO courses (teacher_id,crs_name, crs_duration,crs_cost) VALUES(2,'LITERATURE',1,3500 );
+INSERT INTO courses (teacher_id,crs_name, crs_duration,crs_cost) VALUES(1,'ASTRONOMY',4,5000 );
+INSERT INTO courses (teacher_id,crs_name, crs_duration,crs_cost) VALUES(3,'Accenture Java Training',7,10000 );
+
+
+
+INSERT INTO Modules (module_name, module_marks,course_id) VALUES('math_1',55.5,1 );
+INSERT INTO Modules (module_name, module_marks,course_id) VALUES('english 1a',33,2 );
+INSERT INTO Modules (module_name, module_marks,course_id) VALUES('molecules 2b',57.70,3 );
+INSERT INTO Modules (module_name, module_marks,course_id) VALUES('intro into java',60,4 );
+
+
+INSERT INTO Enrollment (student_id, course_id) VALUES(1,2,2 );
+INSERT INTO Enrollment (student_id, course_id) VALUES(2,2,1 );
+INSERT INTO Enrollment (student_id, course_id) VALUES(1,2,2 );
+INSERT INTO Enrollment (student_id, course_id) VALUES(2,2,3 );
+
+
+
+
--- scripts/src/MA_TABLE_SQL.sql
+++ scripts/src/MA_TABLE_SQL.sql
@@ -1,41 +1,68 @@
 CREATE DATABASE IF NOT EXISTS MA_assignment;
  
 USE MA_assignment;
- 
- CREATE TABLE courses(
-   course_id int not null auto_increment primary key,
-   crs_name varchar(355) not null,
-   crs_duration int  not null,
-   crs_cost int not null
- 
-   );
+
+ CREATE TABLE users
+ (
+  user_id int not null auto_increment primary key,
+  user_name varchar(355) not null,
+  user_email varchar(355) not null,
+  user_password varchar(355) not null,
+  user_type int not null,
+  user_RegisterDate date not null,
+  user_address  text
+ );
 
 
 CREATE TABLE teachers(
    teacher_id int not null auto_increment primary key,
-   course_id int,
-   tch_name varchar(355) not null,
-   tch_lastName varchar(355),
-   tch_Address varchar(355),
+   user_id int not null,
+   teacher_CareerLevel int not null,
+   teacher_DEPT   varchar(255),
  
-   FOREIGN KEY (course_id)
-   REFERENCES courses(course_id)
+   FOREIGN KEY (user_id) REFERENCES users(user_id)
    );
    
    
 CREATE TABLE students(
-   student_id int not null auto_increment primary key,
-    course_id int,
-	 teacher_id int,
-   std_name varchar(255) not null,
-   std_lastName varchar(255),
-   std_address text,
-   FOREIGN KEY (course_id) REFERENCES courses(course_id),
-   
-    FOREIGN KEY (teacher_id) REFERENCES teachers(teacher_id)
+    student_id int not null auto_increment primary key,
+    user_id int not null,
+    student_dept varchar(255),
+
+   FOREIGN KEY (user_id) REFERENCES users (user_id)
    ); 
  
+ CREATE TABLE courses(
+   course_id int not null auto_increment primary key,
+   teacher_id int not null,
+   crs_name varchar(355) not null,
+   crs_duration int  not null,
+   crs_cost int double null,
 
+    FOREIGN KEY (teacher_id) REFERENCES teachers (teacher_id)
+
+   );
+
+CREATE TABLE Enrollment
+(
+    student_id int not null  primary key,
+    course_id int not null primary key,
+
+    FOREIGN KEY (student_id) REFERENCES students (student_id),
+    FOREIGN KEY (course_id) REFERENCES courses (course_id)
+
+);
+
+CREATE TABLE Modules
+(
+   module_id int not null auto_increment primary key,
+   module_name varchar(255),
+   module_marks double,
+   course_id int not null,
+
+    FOREIGN KEY (course_id) REFERENCES courses (course_id),
+
+);
    
 
   
